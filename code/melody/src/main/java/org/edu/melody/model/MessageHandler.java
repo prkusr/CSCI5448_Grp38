3da208ac41268ac7ce7f858e88f02e336ca8d618
Aditya Thyagarajan <aditya1503@gmail.com>
2017-12-08 13:41:10 -0700

    message handler, will add more

---
+++ code/melody/src/main/java/org/edu/melody/model/MessageHandler.java
@@ -0,0 +1,28 @@
+package org.edu.melody.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MessageHandler {
+	
+	protected String message, from, subject;
+
+	public void buildMessage(MessageHandler mes) {
+
+	}
+
+	public boolean send(MessageHandler mes) {
+		return false;
+
+	}
+
+	public void addMessageHeader(MessageHandler mes) {
+
+	}
+}
