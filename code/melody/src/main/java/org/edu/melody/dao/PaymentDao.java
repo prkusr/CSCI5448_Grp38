0da0eacd0e995ceefd841488c68d71fdda42d15f
Aditya Thyagarajan <aditya1503@gmail.com>
2017-12-11 16:37:10 -0700

    payment

--- code/melody/src/main/java/org/edu/melody/dao/PaymentDao.java
+++ code/melody/src/main/java/org/edu/melody/dao/PaymentDao.java
@@ -1,5 +1,112 @@
 package org.edu.melody.dao;
 
-public class PaymentDao {
+import java.sql.Statement;
+import java.time.LocalDate;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.edu.melody.model.CreditCard;
+import org.edu.melody.model.DirectDeposit;
+import org.edu.melody.model.User;
+import java.time.LocalDateTime;
+
+//import scala.annotation.meta.setter;
+
+public class PaymentDao extends AbstractDAO {
+	private static final Logger logger = LogManager.getLogger(PaymentDao.class);
+	
+	
+	public void SetupDirectDeposit(User usr,DirectDeposit dp){
+		try{
+			Statement stmt = null;
+			stmt = getConnection().createStatement();
+			long uid=usr.getUserId();
+			String query = "INSERT INTO DirectDepositDetails VALUES("+uid+
+					",'"+String.valueOf(dp.getAccountNo())+"','"+String.valueOf(dp.getRoutingNumber())+
+					"','"+String.valueOf(dp.getBankName())+"','"
+					+String.valueOf(dp.getBankAddress())+"')";
+			
+			stmt.execute(query);
+		}
+		catch(Exception e){
+			logger.error("Error in saving User Data... ");
+			
+		}
+	}
+	public void SetupCreditCard(User usr,CreditCard dp){
+		try{
+			Statement stmt = null;
+			stmt = getConnection().createStatement();
+			long uid=usr.getUserId();
+			/*long uid=1;
+			CreditCard dp= new CreditCard();
+			dp.setCardNo(12345);
+			dp.setCvv(123);
+			dp.setIssuer("Chase");
+			dp.setExpiry(LocalDateTime.now());
+			dp.setBillingAddress("Mrine Court");*/
+			String query = "INSERT INTO CreditCardDetails VALUES("+uid+
+					",'"+String.valueOf(dp.getCardNo())+"',crypt('"+String.valueOf(dp.getCvv())+
+					"',gen_salt('md5')),'"+String.valueOf(dp.getIssuer())+"',"
+					+String.valueOf(dp.getExpiry().getMonthValue())+","
+					+String.valueOf(dp.getExpiry().getYear())+",'"+
+					dp.getNameOnCard()+"','"+dp.getBillingAddress()+"')";
+			//logger.entry(query);
+			
+			stmt.execute(query);
+		}
+		catch(Exception e){
+			logger.error("Error in saving User Data... ");
+			
+		}
+		
+		
+	}
+	
+	
+	public void DebitUser(User usr,double amount){
+		try{
+			Statement stmt = null;
+			stmt = getConnection().createStatement();
+			long uid=usr.getUserId();
+			//long uid=19;
+			String query = "INSERT INTO PaymentDetails(userId,amount,paymentDate,comments) values("+uid+","+
+					String.valueOf(amount)+",'"+String.valueOf(LocalDate.now())+"',"+
+					"'Success')";
+			stmt.execute(query);
+		}catch(Exception e){
+			
+		}
+	}
+	public void CreditUser(User usr,double amount){
+		try{
+			Statement stmt = null;
+			stmt = getConnection().createStatement();
+			long uid=usr.getUserId();
+			//long uid=19;
+			String query = "INSERT INTO PaymentDetails(userId,amount,paymentDate,comments) values("+uid+","+
+					String.valueOf(-1.0*amount)+",'"+String.valueOf(LocalDate.now())+"',"+
+					"'SuccessArtist')";
+			stmt.execute(query);
+		}catch(Exception e){
+			
+		}
+	}
+	
+	
+	
+	public void test() {
+		// TODO Auto-generated method stub
+try {
+		
+			//SetupDirectDeposit("123","456");
+	//SetupCreditCard("123", "456");
+	//CreditUser("123", 6.99);
+			//loadUserProfile(user);
+			logger.info("Test database debituser");
+
+		} catch (Exception e) {
+			logger.error("Unable to execute Db query", e);
+		}
+	}
 }
