3db99a61e9df088d601b8abb20e6a61e4d2b89d9
Aditya Thyagarajan <aditya1503@gmail.com>
2017-12-10 17:07:44 -0700

    SMS handling first trial

--- code/melody/src/main/java/org/edu/melody/model/TextMessageHandler.java
+++ code/melody/src/main/java/org/edu/melody/model/TextMessageHandler.java
@@ -1,12 +1,21 @@
 package org.edu.melody.model;
-
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
 
 public class TextMessageHandler extends MessageHandler {
 	
 	private long receiverNumber;
+	public static final String ACCOUNT_SID = "AC8af8feedded5bc53aef9816363048148";
+	static final String AUTH_TOKEN = "9c319bfa6f551a352893f0ed488108fd";
 	
 	public void sendMessage(String m){
-		
+		Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+		Message message = Message.creator(new PhoneNumber("+17205464358"), // to
+                new PhoneNumber("+18583675731 "), // from
+                m)
+       .create();
+
 	}
 	public void addMessageHeader(){
 		
